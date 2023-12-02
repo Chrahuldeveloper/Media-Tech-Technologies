3038dc3f5b8341e6c8f9aaddc28abf3c142ab111
Rahul <chrahulofficial@gmail.com>
2023-12-02 19:55:29 +0530

    done blogs

--- src/pages/FullBlog.js
+++ src/pages/FullBlog.js
@@ -1,9 +1,9 @@
 import { useLocation } from "react-router-dom";
 import { Footer } from "../components";
+import React from "react";
 
 const FullBlog = () => {
   const Parms = useLocation();
-
   return (
     <div className="w-screen h-full text-white bg-black">
       <div className="flex flex-col justify-center px-10 py-10 space-y-8 md:px-40">
@@ -12,22 +12,18 @@ const FullBlog = () => {
           className="mx-auto my-5 rounded-lg"
           alt="pic"
         />
-        <h1 className="text-4xl font-bold ">{Parms.state.Blog.head1}</h1>
-        <p className="text-sm font-semibold leading-7 font-">
-          {Parms.state.Blog.Para1}
-        </p>
-        <h1 className="text-4xl font-bold">{Parms.state.Blog.head2}</h1>
-        <p className="text-sm font-semibold leading-7 font-">
-          {Parms.state.Blog.Para2}
-        </p>
-        <h1 className="text-4xl font-bold">{Parms.state.Blog.head3}</h1>
-        <p className="text-sm font-semibold leading-7 font-">
-          {Parms.state.Blog.Para3}
-        </p>
-        <h1 className="text-4xl font-bold">{Parms.state.Blog.head4}</h1>
-        <p className="text-sm font-semibold leading-7 font-">
-          {Parms.state.Blog.Para4}
-        </p>
+        <div>
+          {Parms.state.Blog.Sections?.map((item, i) => {
+            return (
+              <React.Fragment key={i}>
+                <h1 className="text-4xl font-bold ">{item.SectionTittle}</h1>
+                <p className="text-sm font-semibold leading-7 font-">
+                  {item.SectionPara}
+                </p>
+              </React.Fragment>
+            );
+          })}
+        </div>
       </div>
       <Footer />
     </div>
